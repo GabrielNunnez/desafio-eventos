d9aaa3154278c04d9c8da02bd094d9a421541c87
gabriel nuñez <nunez6428@gmail.com>
2022-07-18 17:00:08 -0300

    agregado dom

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let elemento;
+elemento = document;
+console.log(elemento);
 // solicitar el nombre al cliente
 let ingresarNombre = prompt("ingresa su nombre o ESC para interrumpir");
 let texto = " ";
@@ -9,7 +12,7 @@ alert("Hola" + texto + "Bienvenido a nuestra tienda");
 
 //mostrar final del descuento
 const finalDescuento = new Date("july 31, 2022");
-alert("Descuento del 10% finaliza el " + finalDescuento.toLocaleString())
+alert("Descuento del 10% finaliza el " + finalDescuento.toLocaleString());
 
 // solicitar al cliente agregar producto/s para calcular monto total de la compra
 let precioCuchillo = parseFloat(prompt("ingrese precio del primer cuchillo"));
@@ -23,33 +26,57 @@ function sumarProducto(precioCuchillo, precioCuchillo2) {
 console.log(sumarProducto(precioCuchillo, precioCuchillo2));
 
 //costo total
-precioTotal = (sumarProducto(precioCuchillo, precioCuchillo2));
+precioTotal = sumarProducto(precioCuchillo, precioCuchillo2);
 let envio = 500;
 function costoTotal(precioTotal, envio) {
   let totalCompra = precioTotal + envio;
   return totalCompra;
 }
 console.log(costoTotal(precioTotal, envio));
-totalCompra = (costoTotal(precioTotal, envio));
-alert("el total de la compra con el envio es de: " +totalCompra);
+totalCompra = costoTotal(precioTotal, envio);
+alert("el total de la compra con el envio es de: " + totalCompra);
 
-const tiendaCuchillo = {id:1, producto: "cuchillo asado", Tipo: "asado", precio: "5.476"};
-const array =[tiendaCuchillo, {id: 2, producto: "cuchillo alpaca", Tipo: "asado" , precio: "2.476"}];
-array.push({id:3, producto: "cuchillo ciervo", Tipo: "cocina", precio: "6.000"});
-array.push({id:4, producto: "Cuchillos Artesanales Trenzado Alpaca Cincelado", Tipo: "cocina", precio: "3.500"});
+const tiendaCuchillo = {
+  id: 1,
+  producto: "cuchillo asado",
+  Tipo: "asado",
+  precio: 5.476,
+};
+const array = [
+  tiendaCuchillo,
+  { id: 2, producto: "cuchillo alpaca", Tipo: "asado", precio: 2.476 },
+];
+array.push({ id: 3, producto: "cuchillo ciervo", Tipo: "cocina", precio: 6.0 });
+array.push({
+  id: 4,
+  producto: "Cuchillos Artesanales Trenzado Alpaca Cincelado",
+  Tipo: "cocina",
+  precio: 3.5,
+});
 
-console.log(array)
-const filtrado = tiendaCuchillo.find((producto) => producto.Tipo === "asado");
+console.log(array);
+const filtrado = tiendaCuchillo.find((Tipo) => Tipo === "asado");
 console.log(filtrado);
 
 const carrito = [
-  {nombre:"cuchillo asado", precio: 5476},
-  {nombre: "cuchillo alpaca", precio: 2476},
-  {nombre: "cuchillo ciervo", precio: 6000},
-  {nombre:"envio", precio: 500}
+  { nombre: "cuchillo asado", precio: 5476 },
+  { nombre: "cuchillo alpaca", precio: 2476 },
+  { nombre: "cuchillo ciervo", precio: 6000 },
+  { nombre: "envio", precio: 500 },
 ];
 let totalCarrito = carrito.reduce((accum, producto) => {
   return accum + producto.precio;
 }, 0);
-console.log(totalCarrito)
-
+console.log(totalCarrito);
+// agregado productos
+let setParrillero = [
+ { nombre: "set asado x3 piezas", precio: 2500 },
+ { nombre: "set asado x5 piezas", precio: 4000 },
+ { nombre: "set asado x13 piezas", precio: 6000 },
+ ];
+for (setParrillero of setParrillero) {
+let contenedor = document.createElement("div");
+  contenedor.innerHTML = `<h3>Set de parrilla: ${setParrillero.nombre}</h3>
+  <p>precio: ${setParrillero.precio}</p>`;
+  document.body.appendChild(contenedor);
+}

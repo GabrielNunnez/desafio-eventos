31a2b2aa2669ad0064eb915362a0d2ac8bace6e9
gabriel nuñez <nunez6428@gmail.com>
2022-08-01 16:05:25 -0300

    agregando operadores

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaProductos = document.querySelector("#lista-productos");
@@ -66,16 +65,31 @@ function leerDatosProducto(producto) {
 function carritoHTML() {
   limpiarHTML();
   articulosCarrito.forEach((producto) => {
-    const row = document.createElement("tr");
+    let row = document.createElement("tr");
     row.innerHTML = `<td>${producto.titulo}</td>
     <td>${producto.precio}</td>
     <td>${producto.cantidad}</td>
     <td>
     <a href="" class="borrar-producto" id="${producto.id}">x</a>
     </td>`;
-    contenedorCarrito.appendChild(row);
   });
 }
 function limpiarHTML() {
   contenedorCarrito.innerHTML = "";
 }
+const guardarCarrito = contenedorCarrito;
+const enJSON = JSON.stringify(contenedorCarrito);
+localStorage.setItem("guardarCarrito", enJSON);
+// operador and
+const carritoVacio = [];
+carritoVacio.length === 0 && console.log("el carrito esta vacio");
+//destrututando un array
+const itemsTienda = [
+  "id:1, producto: Cuchilla Artesanal Parrilla y Cocina Para Asado, precio: 5.476",
+  "id:2, producto: Cuchillo Artesanal De Campo Disco Arado Acero Asado, precio: 7.000",
+  "id:3, producto: Cuchillo Artesanal Cabo Madera Con Cinco Virolas De Alpaca, precio: 6.000",
+  "id:4, producto: Cuchillos Artesanales Trenzado Alpaca Cincelado, precio: 3.500",
+  "id:5, producto: Juego Asador Kit Asado Set Parrillero X5 Pala Atizado Combo, precio: 5.500",
+];
+const [a] = itemsTienda
+console.log(a)
